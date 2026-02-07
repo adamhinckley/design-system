214c9a2b8caf7a001a45f838a7079a194c0407a9
Adam <adamhinckley@mac.com>
2026-02-06 20:38:14 -0600

    Rename Input export to TextField in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export type { InputProps } from "./components/TextField";
-export { default as Input } from "./components/TextField";
+export { default as TextField } from "./components/TextField";
 export type { PasswordFieldProps } from "./components/PasswordField";
 export { default as PasswordField } from "./components/PasswordField";
